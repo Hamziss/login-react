28edd1a7d861fcaae48c2cd47b9704a23bbb7685
Hamza <hamzachebbah9999@gmail.com>
2022-05-03 07:36:04 +0100

    something

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const colors = require("colors");
 require("dotenv").config();
 const cors = require("cors")
-
+const path = require("path")
 const signUp = require("./routes/signUp")
 const signIn = require("./routes/signIn")
 const addAdmin = require("./routes/addAdmin")
@@ -21,6 +21,19 @@ app.use(express.json())
 app.use("/api/signup", signUp)
 app.use("/api/signin", signIn)
 app.use("/api/admin/add", addAdmin)
+
+//deployment
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+        )
+    )
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.get("/", (req, res) => {
     res.send("API is working")
